e40d782f02a8fd5ed2be0993a3c9180ab916b13b
MPL <manbobo2012@gmail.com>
2020-11-28 20:20:54 +0800

    update

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Simple PHP App</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="hero-unit">
+                <h1>Simple PHP App</h1>
+                <h2>Congratulations!</h2>
+                <p>Your PHP application is now running.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+            </div>
+        </div>
+
+    </body>
+
+</html>
